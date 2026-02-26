25a86dcb801880aebadf5e92dfa9b0588215fea2
brent <rameadtnerbnamzuged@gmail.com>
2026-02-27 00:24:20 +0800

    Update mp4_bilog_daemar.py

--- MP4 - String Operators/mp4_bilog_daemar.py
+++ MP4 - String Operators/mp4_bilog_daemar.py
@@ -7,3 +7,53 @@ last = input("Enter your last name:\n").strip().capitalize()
 formatted_name = f"{last}, {first} {middle[0]}."
 
 print("Formatted Name:", formatted_name)
+
+# 2nd
+
+sentence = input("Enter a sentence:\n")
+
+characters = len(sentence)
+words = len(sentence.split())
+
+vowels = 0
+for char in sentence:
+    if char.lower() in "aeiou":
+        vowels += 1
+
+print("Characters:", characters)
+print("Words:", words)
+print("Vowels:", vowels)
+
+# 3rd
+
+word = input("Enter a word:\n")
+
+if word == word[::-1]:
+    print("The word is a palindrome!")
+else:
+    print("The word is not a palindrome.")
+
+# 4th
+
+phrase = input("Enter a phrase:\n")
+
+result = phrase.upper()[::-1]
+
+print("Output:", result)
+
+#5th
+
+email = input("Enter your email address:\n")
+
+if email.find("@") != -1 and email.find(".") != -1:
+    at_position = email.find("@")
+    username = email[:at_position]
+
+    username = username.lower()
+    username = username.replace(".", " ")
+    username = username.replace("_", " ")
+
+    print("Valid email address!")
+    print("Formatted username:", username)
+else:
+    print("Invalid email address. Please enter a valid email.")
